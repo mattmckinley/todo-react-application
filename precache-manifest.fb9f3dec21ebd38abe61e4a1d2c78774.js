6f3b50724aa1e70b6b0decd1572dd8d05c40a0b7
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-05-08 09:59:51 +0100

    Updates

---
+++ precache-manifest.fb9f3dec21ebd38abe61e4a1d2c78774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d972e0b97188bce312d26391411281",
+    "url": "/todo_react_application/index.html"
+  },
+  {
+    "revision": "80ebc2932ef54e1be1f5",
+    "url": "/todo_react_application/static/css/main.05fce324.chunk.css"
+  },
+  {
+    "revision": "56a61ba83a6eb325bd17",
+    "url": "/todo_react_application/static/js/2.432455d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_react_application/static/js/2.432455d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ebc2932ef54e1be1f5",
+    "url": "/todo_react_application/static/js/main.b19f03d6.chunk.js"
+  },
+  {
+    "revision": "5d4c1a4e2e9a73a8502e",
+    "url": "/todo_react_application/static/js/runtime-main.606d0e57.js"
+  }
+]);
