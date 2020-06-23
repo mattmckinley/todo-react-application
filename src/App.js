7b1b2921863d1ffc3d805d8d91f35bc7af117126
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-06-23 16:00:09 +0100

    removed comment, neatened code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment  } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import './App.css';
 import TaskItem from './TaskItem/TaskItem';
 import TaskInput from './TaskInput/TaskInput';
@@ -10,34 +10,20 @@ import axios from 'axios';
 
 function App() {
 
-  // const [tasks, setTasks] = useState([
-  //   { text: "Feed the cat", completed: false, dueDate: "2020-05-20", id: uuidv4() },
-  //   { text: "Go for a walk", completed: false, dueDate: "2020-06-10", id: uuidv4() },
-  //   { text: "Food shopping", completed: true, dueDate: "2020-05-24", id: uuidv4() },
-  //   { text: "Combine bootstrap with my react to do app", completed: true, dueDate: "2020-05-19", id: uuidv4() },
-  //   { text: "CSS styling", completed: false, dueDate: "2020-06-4", id: uuidv4() }
-  // ]);
-
   const [tasks, setTasks] = useState();
 
   useEffect(() => {
     axios.get("https://kia9m4w721.execute-api.eu-west-1.amazonaws.com/dev/tasks")
-    .then(resp => {
-    // JSON data of the response is available on the .data property resp.data.tasks
-    setTasks(resp.data.tasks);
-  })
-  .catch(err => {
-    console.log("Could not fetch tasks", err);
-  });
+      .then(resp => {
+        setTasks(resp.data.tasks);
+      })
+      .catch(err => {
+        console.log("Could not fetch tasks", err);
+      });
   }, []);
 
   const activeTasks = tasks && tasks.filter(task => !task.completed);
   const completedTasks = tasks && tasks.filter(task => task.completed);
-
-  // function deleteTask(id) {
-  //   const updatedTasks = tasks.filter(task => task.taskId !== id);
-  //   setTasks(updatedTasks);
-  // }
 
   function deleteTask(taskId) {
     axios
@@ -53,13 +39,13 @@ function App() {
 
   function completeTask(id) {
     const updatedTasks = tasks.map(task => {
-      if(task.taskId === id) {
+      if (task.taskId === id) {
         task.completed = true;
       }
       return task;
     })
     setTasks(updatedTasks);
-  }
+  };
 
   function addTask(text, date) {
     const newTask = {
@@ -70,52 +56,51 @@ function App() {
     }
 
     axios
-    .post("https://kia9m4w721.execute-api.eu-west-1.amazonaws.com/dev/tasks", newTask)
-    .then(
-      // If the request is successful, get the task id and add it to the new task object
-      (response) => {
-        newTask.taskId = response.data.data.insertId;
-        const updatedTasks = [ ...tasks, newTask ]
-        setTasks(updatedTasks);
+      .post("https://kia9m4w721.execute-api.eu-west-1.amazonaws.com/dev/tasks", newTask)
+      .then(
+        (response) => {
+          newTask.taskId = response.data.data.insertId;
+          const updatedTasks = [...tasks, newTask]
+          setTasks(updatedTasks);
+        })
+      .catch((error) => {
+        console.log("Error adding a task", error);
       })
-    .catch((error) => {
-      console.log("Error adding a task", error);
-    })
   };
 
   return (
     <div className="App">
       <Header />
       <TaskInput addTask={addTask} />
-      { tasks && (
+      {tasks && (
         <Fragment>
-      <IncompleteRow count={activeTasks.length} />
-      <ul className="task-list">
-        {activeTasks.map(task => {
-          return <TaskItem
-            completeTask={completeTask}
-            deleteTask={deleteTask}
-            id={task.taskId}
-            key={task.taskId}
-            text={task.text}
-            completed={task.completed}
-            dueDate={task.date.substring(0,10)} />
-        })}
-      </ul>
-      <CompleteRow count={completedTasks.length} />
-      <ul className="task-list">
-        {completedTasks.map(task => {
-          return <TaskItem
-            completeTask={completeTask}
-            deleteTask={deleteTask}
-            id={task.taskId}
-            key={task.taskId}
-            text={task.text}
-            completed={task.completed}
-            dueDate={task.date.substring(0,10)} />
-        })}
-      </ul>
-      </Fragment>
+          <IncompleteRow count={activeTasks.length} />
+          <ul className="task-list">
+            {activeTasks.map(task => {
+              return <TaskItem
+                completeTask={completeTask}
+                deleteTask={deleteTask}
+                id={task.taskId}
+                key={task.taskId}
+                text={task.text}
+                completed={task.completed}
+                dueDate={task.date.substring(0, 10)} />
+            })}
+          </ul>
+          <CompleteRow count={completedTasks.length} />
+          <ul className="task-list">
+            {completedTasks.map(task => {
+              return <TaskItem
+                completeTask={completeTask}
+                deleteTask={deleteTask}
+                id={task.taskId}
+                key={task.taskId}
+                text={task.text}
+                completed={task.completed}
+                dueDate={task.date.substring(0, 10)} />
+            })}
+          </ul>
+        </Fragment>
       )}
     </div>
   );
